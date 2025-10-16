d2037394e6a26a72079c966ab5c4fd2b6b9d2ede
adil-java <adiljavaid125@gmail.com>
2025-10-16 02:57:48 -0700

    feat: add all sellers endpoint and improve user role update error messages

--- backend/routes/admin.router.js
+++ backend/routes/admin.router.js
@@ -4,7 +4,8 @@ import {
   getAllUsers,
   getUserById,
   updateUserRole,
-  deleteUser
+  deleteUser,
+  allSellers
 } from '../controllers/admin.controller.js';
 // import {  checkAdmin } from '../middlewares/authMiddleware.js';
 const router = express.Router();
@@ -13,9 +14,10 @@ const router = express.Router();
 
 
 router.route('/login').post(adminLogin); // Placeholder for admin login if needed
+router.route('/user/:id').get(getUserById);
 router.route('/users').get(getAllUsers);
-router.route('/users/:id').get(getUserById);
 router.route('/users/:id/role').put(updateUserRole);
 router.route('/users/:id').delete(deleteUser);
+router.route("/sellers").get( allSellers);
 
 export default router;
