6d11733d893968c8786e72cad814064e66c698eb
adil-java <adiljavaid125@gmail.com>
2025-11-06 19:12:20 +0500

    ...

--- backend/utils/firebaseAdmin.js
+++ backend/utils/firebaseAdmin.js
@@ -1,20 +1,4 @@
-const requiredEnv = [
-  'FIREBASE_TYPE',
-  'FIREBASE_PROJECT_ID',
-  'FIREBASE_PRIVATE_KEY_ID',
-  'FIREBASE_PRIVATE_KEY',
-  'FIREBASE_CLIENT_EMAIL',
-  'FIREBASE_CLIENT_ID',
-  'FIREBASE_AUTH_URI',
-  'FIREBASE_TOKEN_URI',
-  'FIREBASE_AUTH_PROVIDER_X509_CERT_URL',
-  'FIREBASE_CLIENT_X509_CERT_URL'
-];
-for (const key of requiredEnv) {
-  if (!process.env[key]) {
-    throw new Error(`Missing env var: ${key}`);
-  }
-}
+
 
 const serviceAccount = {
   "type": process.env.FIREBASE_TYPE,
