bd6b0b370bddd1fd749c33ef08a2b4397b49af66
adil-java <adiljavaid125@gmail.com>
2025-11-06 17:45:49 +0500

    firebase links in the  firenase

--- backend/utils/firebaseAdmin.js
+++ backend/utils/firebaseAdmin.js
@@ -1,10 +1,8 @@
 import admin from "firebase-admin";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { readFileSync } from "fs";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+
+
+
 
  const serviceAccount = {
   "type": process.env.FIREBASE_TYPE,
